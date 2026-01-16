1d0063cf350ca4becaedc895bb2d0a1735178bb9
Ojaswa Varshney <118374998+JrG-One@users.noreply.github.com>
2026-01-16 16:32:01 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./styles/animations.css";
 import "./App.css";
@@ -7,11 +7,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/Ojaswa-s-Portfolio">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
